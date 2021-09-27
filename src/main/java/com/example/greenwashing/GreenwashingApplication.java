1537282bcc2c68c25f01068367b17200e5324c6a
Théo <theo.mouton@axopen.com>
2021-09-27 12:00:57 +0200

    fix ?

--- src/main/java/com/example/greenwashing/GreenwashingApplication.java
+++ src/main/java/com/example/greenwashing/GreenwashingApplication.java
@@ -6,14 +6,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.example.greenwashing.*")
-@EntityScan(basePackages = "greenwashing")
-@ComponentScan("com.example.greenwashing.model")
+@EntityScan({"io.summer", "com.example.greenwashing.model"})
 @SpringBootApplication
 public class GreenwashingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreenwashingApplication.class, args);
 	}
-
 }
